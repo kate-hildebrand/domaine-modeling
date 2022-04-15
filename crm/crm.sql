d8c128e25fe1c7991655d09054ed8add53a72dde
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-04-15 20:41:38 +0000

    CRM Project

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,51 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
--- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS my_contacts;
+DROP TABLE IF EXISTS enrollments;
 
 -- CREATE TABLES
+
+--create base tables 
+
+CREATE TABLE contacts (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   contact_name TEXT,
+   email_address TEXT,
+   phone_number TEXT
+);
+
+CREATE TABLE activities (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     contact_type TEXT,
+     contact_date DATE,
+     contact_time TIME
+);
+
+CREATE TABLE companies (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     company_name TEXT
+);
+
+CREATE TABLE my_contacts (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     first_name TEXT,
+     last_name TEXT,
+     email_address TEXT
+);
+
+--Create joined tables 
+CREATE TABLE company_contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
+    contact_id INTEGER
+);
+
+
+CREATE TABLE my_activities (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    activities_id INTEGER,
+    my_contacts_id INTEGER
+);
